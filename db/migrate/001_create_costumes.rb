389c505c78b6966df9867b3aa10bc183e5c2bba7
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-17 16:30:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,9 +2,10 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.float :price 
-      t.float :size 
+      t.decimal :price 
+      t.decimal :size 
       t.string :image_url
+      t.
     end
   end
   
