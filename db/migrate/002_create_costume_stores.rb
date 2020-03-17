2ef57e906ff97be7977cc08f15ea9a1b53c89956
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-17 17:14:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location 
       t.integer :costume_inventory 
       t.integer :number_of_employees
-      t.boolean :in_business
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
