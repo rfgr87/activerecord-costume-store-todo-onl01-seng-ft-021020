5ebfa67423d87388ec517051a521591ae85bdbed
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-17 16:25:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes 
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price 
+      t.float :size 
+      t.string :image_url
+    end
+  end
   
 end
 
